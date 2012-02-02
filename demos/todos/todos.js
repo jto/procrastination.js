a1b25226b0acce5b9dfb386a8c31f0bd7dd5f200
jto <jto@zenexity.com>
2012-02-02 18:20:54 +0100

    First experiments, TODO: How should I deal with views ??

--- demos/todos/todos.js
+++ demos/todos/todos.js
@@ -1,57 +1,106 @@
 $(function(){
 	
-	// Models
-	function Todo(value){ 
-		return 
+	/**
+	* Utilities Actions
+	* Should be provided by procrastination.js
+	*/
+	function PUT(url){
+		return new Action(function(data, next){
+			console.log('PUT %o', data)
+			setTimeout(function(){ next(data) }, 500)
+			//$.put(url, data, next)
+		})
 	}
 	
-	// Effects
-	// TODO: find a name fot that (Task ?)
-	var save = new Action()
-		.map(...)
-		.foreach(...)
-	
-	var addToList = new Action(function(todo){
-			return _.template($('#item-template').html())(t)
+	function POST(url){
+		return new Action(function(data, next){
+			console.log('POST %o', data)
+			setTimeout(function(){ next(data) }, 500)
+			//$.put(url, data, next)
 		})
-		.foreach(function(){
-			
-		})
-
-	// Action()
-	var spinner = new Action(showSpinner)
-		.onComplete(removeSpinner) 
-
-	var cleanForm = new Action()
-		.foreach()
-
-	function gimmeSpin('#toto'){
-		return new Action(...)
 	}
 
-	var saveAndAdd = addToList
-		.and(gimmeSpin('#wait'))
-		.and(cleanForm)
-		.await(save)
-
-	// Sources
-	var values = new Reactive(function (next){ 
-		next($('#new-todo').val())
+	var Append = new Action(function(view, next){
+		view.render()
+		next(view)
 	})
+	
+	/**
+	* Views
+	*/
+	var TodoIn = {
+		el: $('#new-todo'),
 
-	var source = new Reactive(function (next){ 
-			$('#new-todo').keydown(next)
+		value: new Action(function(v, next){ 
+			next(TodoIn.el.val())
+		}),
+
+		clear: new Action(function(v, next){
+			TodoIn.el.val('')
+			next(v)
+		}),
+		
+		key: function(next){ $('#new-todo').keydown(next, false) }
+	}
+	
+	function Line(todo){
+		this.el = undefined
+		this.parent = $('#todo-list')
+		this.tmpl = _.template($('#item-template').html())
+		this.todo = todo
+	}
+	Line.prototype = {
+		render: function(){
+			this.el = $(this.tmpl(this.todo)).appendTo(this.parent)
+			return this
+		},
+		
+		del: function(next){
+			$('.todo-destroy', this.el).click(next)
+		},
+		
+		postpone: function(next){
+			$('.check', this.el).click(next)
+		}
+	}
+
+	/**
+	* Actions
+	*/
+	var Spinner = {
+		show: new Action(function(v, next){
+			console.log('doing stuff')
+			next(v)
+		}),
+		
+		hide: new Action(function(v, next){
+			console.log('Finished')
+			next(v)
 		})
+	}
+
+	var NewTodo = Append
+		.and(TodoIn.clear)
+		.and(Spinner.show)
+		.and(PUT('/todo'))
+		.then(Spinner.hide)
+
+	var Postpone = POST('/todo')
+		.and(Spinner.show)
+		.then(Spinner.hide)
+
+	/**
+	* Events
+	*/
+	new Reactive()
+		.on(TodoIn.key)
 		.filter(function(evt){
 			return evt.keyCode == 13
 		})
-		.await(saveAndAdd)
-		.map(function(v){ 
-			return Todo(v[1])
+		.await(TodoIn.value)
+		.map(function(v){
+			return new Line({ text: v, done: false, since: new Date()})
 		})
-
-	// Controller
-	source.await(saveAndAdd)
+		.await(NewTodo)
 		.subscribe()
-	
 })
